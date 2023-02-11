ce45533c9ae1828e1b3825131ddbf04912f889f6
saeedkaz <saeed.kazemy@gmail.com>
2023-02-12 00:27:10 +0330

    db finished

--- go-movies-back-end/cmd/api/main.go
+++ go-movies-back-end/cmd/api/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"backend/internal/repository"
+	"backend/internal/repository/dbrepo"
+	"flag"
 	"fmt"
 	"log"
 	"net/http"
@@ -9,7 +12,9 @@ import (
 const port = 8080
 
 type application struct {
+	DSN    string
 	Domain string
+	DB     repository.DatabaseRepo
 }
 
 func main() {
@@ -17,16 +22,23 @@ func main() {
 	var app application
 
 	// read from command line
+	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "Postgres connection string")
+	flag.Parse()
 
 	// connect to the database
+	conn, err := app.connctToDB()
+	if err != nil {
+		log.Fatal(err)
+	}
+	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
+	defer app.DB.Connection().Close()
 
 	app.Domain = "example.com"
 
 	log.Println("Starting application on port", port)
 
-
 	// start a web server
-	err := http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
+	err = http.ListenAndServe(fmt.Sprintf(":%d", port), app.routes())
 	if err != nil {
 		log.Fatal(err)
 	}
